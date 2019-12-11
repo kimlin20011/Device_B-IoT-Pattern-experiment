71a9c1a976ad260abbd45e57459827f4570ab576
林俊安 <https://github.com/kimlin20011>
2019-12-11 16:35:07 +0800

    add pid usage file

--- controllers/experiments.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    async pid(ctx) {
-            ctx.body = process.pid;
-    },
-}
--- routers/experiments_router.js
+++ routers/experiments_router.js
@@ -1,5 +1,6 @@
 const router = require('koa-router')();
-const exp = require('../controllers/experiments');
+const exp = require('../controllers/exp_controller');
 
 module.exports = router
-    .get('/getPID', exp.pid)
+    //.get('/getPID', exp.pid)
+    .get('/getPIDusage', exp.getPIDusage)

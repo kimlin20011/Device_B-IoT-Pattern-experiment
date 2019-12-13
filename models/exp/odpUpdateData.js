1d0a654a59abed1c2e37e7c5081737f7d6c4160a
林俊安 <https://github.com/kimlin20011>
2019-12-13 15:57:58 +0800

    v1

--- models/exp/odpUpdateData.js
+++ models/exp/odpUpdateData.js
@@ -12,6 +12,17 @@ module.exports = async function listenQueryEvent(data) {
     let result = {};
     result.signature = data.signature;
     result.topic = data.topic;
+    let timestamp = Date.now();
+    let str = `${timestamp}\n`;
+    try {
+        fs.appendFile(`./logs/${data.csvName}.csv`, str, function (err) {
+            if (err) throw err;
+            console.log(`${data.csvName} Log Saved!`);
+        });
+    } catch (e) {
+        console.log(e);
+        fs.writeFileSync(`./logs/${data.csvName}.csv`, str, (err) => { console.log(err); });
+    }
 
     for (let i = 0; i < data.queryTime; i++) {
         request.post({
